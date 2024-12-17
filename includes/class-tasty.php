44f02ccb8484fa94cbeb6ebe0b65c61915d93de3
md-hiron <hironmd647@gmail.com>
2024-12-17 20:27:59 +0600

    taxonomies code minimise

--- includes/class-tasty.php
+++ includes/class-tasty.php
@@ -166,101 +166,17 @@ class Tasty {
 
 		$this->loader->add_action( 'admin_menu', $tasty_user_report, 'user_report_pages' );
 
-		/**
-		 * add custom field to custom taxnomy
-		 * This code used for temporary. It will reduced next time
-		 */
-		$this->loader->add_action( 'style_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'color_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'material_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'size_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'shape_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'features_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'room-type_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'price-category_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'mood-theme_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'usage_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
-		$this->loader->add_action( 'functionality_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
+		//Taxonomies array
+		$all_tags = array( 'style', 'color', 'material', 'size', 'shape', 'features', 'room-type', 'price-category', 'mood-theme', 'usage', 'functionality' );
 
-		/**
-		 * Edit custom field form of custom taxonomy adding into hook
-		 * his code used for temporary. It will reduced next time
-		 */
-		$this->loader->add_action( 'style_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'color_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'material_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'size_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'shape_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'features_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'room-type_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'price-category_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'mood-theme_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'usage_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-		$this->loader->add_action( 'functionality_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
-
-		/**
-		 * Create custom field form adding into hook
-		 * This code used for temporary. It will reduced next time
-		 */
-		$this->loader->add_action( 'created_style', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_color', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_material', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_size', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_shape', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_features', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_room-type', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_price-category', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_mood-theme', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_usage', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'created_functionality', $tasty_custom_field, 'save_custom_meta_for_tax' );
-
-		/**
-		 * udpate customer data adding into hook
-		 * This code used for temporary. It will reduced next time
-		 */
-		$this->loader->add_action( 'edited_style', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_color', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_material', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_size', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_shape', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_features', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_room-type', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_price-category', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_mood-theme', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_usage', $tasty_custom_field, 'save_custom_meta_for_tax' );
-		$this->loader->add_action( 'edited_functionality', $tasty_custom_field, 'save_custom_meta_for_tax' );
-
-		/**
-		 * ad customer column adding into hook
-		 * This code used for temporary. It will reduced next time
-		 */
-		$this->loader->add_filter( 'manage_edit-style_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-color_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-material_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-size_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-shape_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-features_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-room-type_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-price-category_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-mood-theme_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-usage_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-		$this->loader->add_filter( 'manage_edit-functionality_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
-
-		/**
-		 * ad customer column adding into hook
-		 * This code used for temporary. It will reduced next time
-		 */
-		$this->loader->add_filter( 'manage_style_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_color_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_material_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_size_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_shape_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_features_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_room-type_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_price-category_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_mood-theme_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_usage_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
-		$this->loader->add_filter( 'manage_functionality_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
+		foreach( $all_tags as $tag ){
+			$this->loader->add_action( $tag . '_add_form_fields', $tasty_custom_field, 'add_custom_meta_for_tax' );
+			$this->loader->add_action( $tag . '_edit_form_fields', $tasty_custom_field, 'edit_custom_meta_for_tax' );
+			$this->loader->add_action( 'created_' . $tag, $tasty_custom_field, 'save_custom_meta_for_tax' );
+			$this->loader->add_action( 'edited_' . $tag, $tasty_custom_field, 'save_custom_meta_for_tax' );
+			$this->loader->add_filter( 'manage_edit-'. $tag .'_columns', $tasty_custom_field, 'add_custom_meta_in_tax_column' );
+			$this->loader->add_filter( 'manage_'. $tag .'_custom_column', $tasty_custom_field, 'show_column_value_in_tax', 10, 3 );
+		}
 	}
 
 	/**
