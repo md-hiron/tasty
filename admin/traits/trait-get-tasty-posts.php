796b595ee9ec2b1c9211635f495b81cb3b3c20ee
md-hiron <hironmd647@gmail.com>
2025-02-12 16:17:07 +0600

    tag weight algorythm for post

--- admin/traits/trait-get-tasty-posts.php
+++ admin/traits/trait-get-tasty-posts.php
@@ -47,7 +47,7 @@ trait Get_Tasty_Posts{
      * @version 1.0.0
      * @access  public
      */
-    public function get_tasty_posts( $rquest ){
+    public function get_tasty_posts( $request ){
         global $wpdb;
         
         //current user ID
@@ -68,69 +68,73 @@ trait Get_Tasty_Posts{
             }
         }
 
-        //initially load 5 posts. When user swiped 3 item them load 3 images only
-        $swiped_ids     = !empty( $rquest['swiped_ids'] ) ? $rquest['swiped_ids'] : [];
-        $loaded_ids     = !empty( $rquest['loaded_ids'] ) ? $rquest['loaded_ids'] : [];
+        
 
-        //Initail posts per page
-        $posts_per_page = 5;
-
-        //Posts per page on next fetch
-        if( $swiped_ids ){
-            $posts_per_page = 3;
-        }
+        
         
         // Fetch swiped posts for the user
         $column_name       = $user_id ? 'user_id' : 'app_user_id';
         $user_identifier   = $user_id ?: $app_user_id;
 
+        //initially load 5 posts. When user swiped 3 item them load 3 images only
+        $swiped_ids     = !empty( $request['swiped_ids'] ) ? $request['swiped_ids'] : [];
+        $loaded_ids     = !empty( $request['loaded_ids'] ) ? $request['loaded_ids'] : [];
+
         //Post IDs that's already chosed by the user.
         $chose_post_ids    = $this->chose_post_ids( $user_identifier, $column_name );
-
         $excludes_posts    = array_unique( array_merge( $chose_post_ids, $loaded_ids ) );
+
+        //Post per page and order based on user action
+        $posts_per_page = $swiped_ids ? 3 : 5;
+        $orderby = $swiped_ids ? 'date' : 'rand';
+
+        
         
         $primary_args = array(
             'post_type'      => 'post',
             'posts_per_page' => $posts_per_page,
             'post_status'    => 'publish',
-            'orderby'        => 'rand',
+            'orderby'        => $orderby,
             'tax_query'      => $this->tasty_tax_query( $user_identifier, $column_name ), //tax query from the tasty tax query method
             'post__not_in'   => $excludes_posts, // Exclude previously swiped posts
         );
 
         // Fetch posts
         $primary_query = new WP_Query( $primary_args );
-        $found_posts   = $primary_query->found_posts;
 
-        $fetched_ids    = wp_list_pluck( $primary_query->posts, 'ID' );
+        
+        //user tag weight
+        $user_tag_weights = $this->get_user_tag_weight( $user_identifier, $column_name );
+        //Rank post using stored tag score
+        $ranked_posts = $this->rank_posts_by_tag_weight( $primary_query->posts, $user_tag_weights );
+
+        // exlode post those are fetched already and in ranked post
+        $fetched_ids    = wp_list_pluck( $ranked_posts, 'ID' );
         $excludes_posts = array_merge( $excludes_posts, $fetched_ids );
 
-        //Initial combined posts
-        $combined_posts = $primary_query->posts;
-
         //if fewer post than needed, fetech additional posts without term
-        if( $found_posts < $posts_per_page ){
-            $remaining_posts_needed = $posts_per_page - $found_posts;
+        if( count( $ranked_posts ) < $posts_per_page ){
+            $remaining_posts_needed = $posts_per_page - count( $ranked_posts );
 
             //secondary args without tax query
-            $secondary_args = array(
+            $fallback_args = array(
                 'post_type'      => 'post',
-                'posts_per_page' => $posts_per_page,
+                'posts_per_page' => $remaining_posts_needed,
                 'post_status'    => 'publish',
                 'orderby'        => 'rand',
                 'post__not_in'   => $excludes_posts, // Exclude previously swiped posts
             );
 
-            $secondary_query = new WP_Query( $secondary_args );
+            $fallback_query = new WP_Query( $fallback_args );
 
-            $combined_posts = array_merge( $combined_posts, $secondary_query->posts );
+            $ranked_posts = array_merge( $ranked_posts, $fallback_query->posts );
 
         }
 
         // Prepare response
         $posts = [];
 
-        if ( !empty( $combined_posts ) ) {
+        if ( !empty( $ranked_posts ) ) {
             $posts = array_map( function( $post ) {
                 return array(
                     'id'             => $post->ID,
@@ -138,10 +142,11 @@ trait Get_Tasty_Posts{
                     'featured_image' => get_the_post_thumbnail_url( $post->ID, 'full' ),
                     'addition_info'  => !empty( get_post_meta( $post->ID, 'addi_info', true ) ) ? get_post_meta( $post->ID, 'addi_info', true ) : null
                 );
-            }, $combined_posts );
+            }, $ranked_posts );
         }
         
         return new WP_REST_Response( $posts, 200 );
+        
     }
 
     /**
@@ -308,6 +313,81 @@ trait Get_Tasty_Posts{
         // Merge provided IDs and latest IDs
         return $latest_ids;
     }
-    
 
+    /**
+     * Fetch user specific tag weight
+     * 
+     * @param   int     $user_identifier    The user ID or app user ID
+     * @param   string  $column_name        Column name of user ID or App user ID
+     * @param   array  $provided_ids       The ids that user swipped 3 times
+     * 
+     * @since 1.0.0
+     * @access  private
+     */
+    private function get_user_tag_weight( $user_identifier, $column_name ){
+        //check args are provided
+        if( empty( $user_identifier ) || empty( $column_name ) ){
+            return;
+        }
+
+        global $wpdb;
+
+        $weights = $wpdb->get_results(
+            $wpdb->prepare(
+                "SELECT tag_id, taxonomy, tag_weight_score as score
+                FROM {$wpdb->prefix}{$this->tag_weight_table} WHERE 
+                $column_name = %d",
+                $user_identifier
+            )
+        );
+
+        $tag_weights = [];
+        foreach( $weights as $weight ){
+            $tag_weights[ $weight->tag_id ] = (float) $weight->score;
+        }
+
+        return $tag_weights;
+    }
+
+    /**
+     * Rank posts by tag weight
+     * 
+     * @param   object  $posts              filtered post object for ranking
+     * @param   array   $user_tag_weight    tag weights array against tag IDs
+     * 
+     * @since   1.0.0
+     * @access  private
+     */
+    private function rank_posts_by_tag_weight( $posts, $user_tag_weight ){
+        //check args are provided
+        if( empty( $posts ) || empty( $user_tag_weight ) ){
+            return [];
+        }
+
+        $ranked_posts = [];
+
+        foreach( $posts as $post ){
+            $score = 0;
+
+            // Get post terms
+            $post_terms = wp_get_post_terms( $post->ID, Tasty_Helper::get_tasty_tags(), ['fields' => 'ids'] );
+
+            //calculate score based on stored user tag weight
+            foreach( $post_terms as $term_id ){
+                if( isset( $user_tag_weight[$term_id] ) ){
+                    $score += $user_tag_weight[$term_id];
+                }
+            }
+
+            $ranked_posts[] = array( 'post' => $post, 'score' => $score );
+        }
+
+        //sore post by tags weight
+        usort( $ranked_posts, function( $a, $b ){
+            return $b['score'] <=> $a['score'];
+        } );
+
+        return array_column( $ranked_posts, 'post' );
+
+    }
 }
